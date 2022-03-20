99aae9dede0d43e1005174f5a41f1d68200747d7
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2022-03-20 17:47:25 -0300

    feature: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ODS 10 API')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api/docs', app, swaggerDoc);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
